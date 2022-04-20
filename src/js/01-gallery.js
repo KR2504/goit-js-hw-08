44749fb86a75585a36f6c46ad6f7d5a51944b7f8
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-04-20 23:39:27 +0300

    js-hw-8-tasks-1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const markupGallery = createGallery(galleryItems)
+
+gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+function createGallery(pictures) {
+    return pictures.map(({ preview, original, description }) => {
+        return `<a class="gallery__item"
+        href="${original}">
+            <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+            </a>`
+    }).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const throttleOnVideoPlayerTime = throttle(savePlaybackTime, 1000);
+
+player.on('timeupdate', throttleOnVideoPlayerTime);
+
+function savePlaybackTime(currentTime) {
+    const currentTimeJSON = JSON.stringify(currentTime.seconds)
+    localStorage.setItem(LOCALSTORAGE_KEY, currentTimeJSON);
+}
+
+function setCurrentTime() {
+    const getTIme = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    player.setCurrentTime(getTIme);
+}
+
+setCurrentTime();
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
